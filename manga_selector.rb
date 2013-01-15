1aa919b61fb601d1b64442c55e2bc408e3ed9cf5
Thiago Assis <thiagosil@gmail.com>
2013-01-15 19:12:09 -0200

    adding layout to all pages

--- manga_selector.rb
+++ manga_selector.rb
@@ -4,16 +4,18 @@ require_relative 'mangareader'
 
 #moofle
 
+set :haml, :format => :html5
+
 get '/' do
   manga_list
-  haml :select_manga, :format => :html5 
+  haml :select_manga
 end
 
 get '/mangas/:manga_name' do
   @mr = Mangareader.new
   @mangas = @mr.list_mangas
   @chapters = @mr.list_chapters(params[:manga_name])
-  haml :select_chapters, :format => :html5 
+  haml :select_chapters
 end
 
 get '/main' do
